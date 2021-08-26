85b189ed6bfb2008577f8815cf4456bdeccde64d
shajjad-shuvo-cit <shajjad.cit.bd@gmail.com>
2021-08-26 16:14:22 +0600

    banner part ends

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(function(){
+    //banner chat slider;
+    $('.banner-chat-slider').slick({
+        arrows: false,
+        vertical: true,
+        slidesToShow:4,
+        slidesToScroll: 1,
+        autoplay: true,
+    });
+});
